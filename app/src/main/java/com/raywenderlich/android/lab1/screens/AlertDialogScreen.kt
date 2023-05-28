484eb341b4d9083e8a882852c80563fe88fc6de4
Киря <kirilldorofej@gmail.com>
2023-05-28 22:05:31 +0300

    Вспдывающее диалоговое окно

--- app/src/main/java/com/raywenderlich/android/lab1/screens/AlertDialogScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/AlertDialogScreen.kt
@@ -1,6 +1,12 @@
 package com.raywenderlich.android.lab1.screens
-
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -17,5 +23,36 @@ fun AlertDialogScreen() {
 
 @Composable
 fun MyAlertDialog() {
+    val shouldShowDialog = remember { mutableStateOf(true) } // 1
+
+    if (shouldShowDialog.value) { // 2
+        AlertDialog( // 3
+            onDismissRequest = { // 4
+                shouldShowDialog.value = false
+                JetFundamentalsRouter.navigateTo(Screen.Navigation)
+            },
+            // 5
+            title = { Text(text = stringResource(id = "Внимание")) },
+            text = { Text(text = stringResource(id = "Это диалоговое окно")) },
+            confirmButton = { // 6
+                Button(
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue),
+                    onClick = {
+                        shouldShowDialog.value = false
+                        JetFundamentalsRouter.navigateTo(Screen.Navigation)
+                    }
+                ) {
+                    Text(
+                        text = stringResource(id = "Закрыть"),
+                        color = Color.White
+                    )
+                }
+            }
+        )
+    }
     //TODO add your code here
+}
+
+fun stringResource(id: String): String {
+    TODO("Not yet implemented")
 }
